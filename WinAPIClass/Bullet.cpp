ea0baad6ba08b47680aea74666c6741a4796064d
준영 고 <wl7432@naver.com>
2024-05-09 13:56:49 +0900

    총알수정

--- WinAPIClass/Bullet.cpp
+++ WinAPIClass/Bullet.cpp
@@ -6,11 +6,23 @@
 
 void Bullet::Update()
 {
+	// 지속 시간이 지난 총알을 비활성화 시키고 다시 큐에 넣는다.
+	// Plane 클래스에서 Shoot()을 실행하면 다시 발사 가능하다.
+	if (lifeTimeCheck >= lifeTime)
+	{
+		lifeTimeCheck = 0.0f;
+		bulletPool.push(this);
+		SetActive(false);
+	}
+	else
+	{
+		lifeTimeCheck += Time::GetDeltaTime();
+	}
 	Move(dir * moveSpeed * Time::GetDeltaTime());
 }
 
 Bullet::Bullet(queue<Bullet*>& bulletPool)
-	: dir({ 1.0f,0.0f }), moveSpeed(1000.0f), bulletPool(bulletPool)
+	: dir({ 1.0f,0.0f }), moveSpeed(1000.0f), bulletPool(bulletPool), lifeTime(2.0f), lifeTimeCheck(0.0f)
 {
 	renderer = new AtlasRenderer(IDB_BULLET, *this, 5, RGB(255, 255, 255));
 	renderer->SetSize(30, 40);
--- WinAPIClass/Bullet.h
+++ WinAPIClass/Bullet.h
@@ -15,6 +15,8 @@ private :
 private : 
 	Vector2 dir; // 총알이 날아가는 방향
 	float moveSpeed;//총알이 날아가는 속도
+	float lifeTime; // 지속 시간
+	float lifeTimeCheck; // 시간 측정 변수
 
 public :
 	virtual void Update();
